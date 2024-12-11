5bebe2ef9fea27995d78d6be4c5603b5cbe56f8d
Eric-Av <albina.gorbatenko56@mail.ru>
2024-12-11 23:57:46 +0300

    финальный финал (надеюсь)

---
+++ final/система бронирования путешествий/loginn.js
@@ -0,0 +1,27 @@
+const firstNameInput = document.getElementById('first_name')
+const lastNameInput = document.getElementById('last_name')
+const titleInput = document.getElementById('title')
+const descriptionInput = document.getElementById('description')
+const button = document.getElementById('button')
+
+$.get('https://technium-frontend-intensive.replit.app', function(data) {
+  firstNameInput.value = data.first_name
+  lastNameInput.value = data.last_name
+  emailInput.value = data.email
+  titleInput.value = data.title
+  descriptionInput.value = data.description
+})
+// https://technium-frontend-intensive.replit.app
+
+button.addEventListener('click', function() {
+
+  $.post('https://technium-frontend-intensive.replit.app', {
+    first_name: firstNameInput.value,
+    last_name: lastNameInput.value,
+    email: emailInput.value,
+    title: titleInput.value,
+    description: descriptionInput.value,
+  }, function() {
+    alert("Данные сохранены")
+  })
+})
